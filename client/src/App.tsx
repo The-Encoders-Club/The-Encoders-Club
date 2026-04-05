6fda572b436fa3a5a3fbb1174d5631ff9d110506
Francisco50-Discord <francisco20javier600@gmail.com>
2026-04-05 16:38:04 -0600

    Actualizar App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,20 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
-import SplashScreen from "./components/SplashScreen";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import Proyectos from "./pages/Proyectos";
-import ProyectoDetalle from "./pages/ProyectoDetalle";
 import Cursos from "./pages/Cursos";
 import Noticias from "./pages/Noticias";
 import Donar from "./pages/Donar";
-import { useState, useEffect } from "react";
+import SplashScreen from "./components/SplashScreen";
+import BackgroundParticles from "./components/BackgroundParticles";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/proyectos" component={Proyectos} />
-      <Route path="/proyectos/:id" component={ProyectoDetalle} />
       <Route path="/cursos" component={Cursos} />
       <Route path="/noticias" component={Noticias} />
       <Route path="/donar" component={Donar} />
@@ -29,24 +27,13 @@ function Router() {
 }
 
 function App() {
-  const [showSplash, setShowSplash] = useState(true);
-
-  useEffect(() => {
-    // Only show splash on first load
-    const hasSeenSplash = sessionStorage.getItem("hasSeenSplash");
-    if (hasSeenSplash) {
-      setShowSplash(false);
-    } else {
-      sessionStorage.setItem("hasSeenSplash", "true");
-    }
-  }, []);
-
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
-          {showSplash && <SplashScreen onComplete={() => setShowSplash(false)} />}
           <Toaster />
+          <SplashScreen />
+          <BackgroundParticles />
           <Router />
         </TooltipProvider>
       </ThemeProvider>
