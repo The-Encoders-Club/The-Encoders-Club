39f3755ad0d7395b4b167d1379291bc0869926c1
Francisco50-Discord <francisco20javier600@gmail.com>
2026-04-06 11:14:05 -0600

    Actualizar NatsukiProjectView.tsx

--- client/src/components/NatsukiProjectView.tsx
+++ client/src/components/NatsukiProjectView.tsx
@@ -23,8 +23,8 @@ const NatsukiProjectView: React.FC<ProjectViewProps> = ({ isOpen, onClose }) => 
 
   if (!isOpen) return null;
 
-  // Portada correcta: Just Natsuki.png
-  const mainImage = "https://files.manuscdn.com/user_upload_by_module/session_file/310519663518113549/lqfZTkbzoCrwSxwy.png";
+  // URLs de Cloudfront autorizadas
+  const mainImage = "https://d2xsxph8kpxj0f.cloudfront.net/310519663516100892/kzug5rLPLvVJzu5QVE66vY/IMG_20260405_121731_665_166b9420.jpg";
   const logoImage = "https://d2xsxph8kpxj0f.cloudfront.net/310519663516100892/kzug5rLPLvVJzu5QVE66vY/logo_435f8d5a.png";
 
   return (
