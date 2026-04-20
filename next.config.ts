5c767dde608cd453bf6a7a37b166f2a55c1653df
«[×𝐹ɾαɳƈιʂƈσ×]» <francisco20javier600@gmail.com>
2026-04-20 15:43:38 -0600

    Actualizar next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
+import { setupDevBindings } from "@cloudflare/next-on-pages/next";
+if (process.env.NODE_ENV === "development") setupDevBindings();
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
