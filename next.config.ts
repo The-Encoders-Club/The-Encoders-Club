980b456491ec82569ccf633d4328510e91906d57
«[×𝐹ɾαɳƈιʂƈσ×]» <francisco20javier600@gmail.com>
2026-04-21 20:15:47 -0600

    Actualizar next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: false,
-  // Configuración para Cloudflare Pages
-  experimental: {
-    runtime: "nodejs",
-  },
-  // Asegurarse de que las variables de entorno se pasen correctamente
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
 };
 
 export default nextConfig;
