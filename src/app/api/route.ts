09dfd1a58df20215e21b155ad7ceda3edd6028ce
«[×𝐹ɾαɳƈιʂƈσ×]» <francisco20javier600@gmail.com>
2026-04-25 10:21:17 -0600

    Actualizar route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({ message: "Hello, world!" });
+  return NextResponse.json({
+    message: 'Welcome to The Encoders Club API',
+    version: '2.0.0',
+    runtime: 'Cloudflare Workers',
+  });
 }
