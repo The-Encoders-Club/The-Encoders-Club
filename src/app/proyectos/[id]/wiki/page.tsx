4d427d6430e4ec984855a4a8ad6266b10d9a7779
«[×𝐹ɾαɳƈιʂƈσ×]» <francisco20javier600@gmail.com>
2026-05-27 12:51:00 -0600

    Actualizar page.tsx

--- src/app/proyectos/[id]/wiki/page.tsx
+++ src/app/proyectos/[id]/wiki/page.tsx
@@ -20,6 +20,7 @@ import {
   X,
 } from 'lucide-react';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { projectWikiData } from '@/data/wikiContent';
@@ -1060,7 +1061,7 @@ export default function WikiPage() {
 
           {/* Wiki Article */}
           <article className="wiki-article">
-            <ReactMarkdown components={markdownComponents}>
+            <ReactMarkdown remarkPlugins={[remarkGfm]} components={markdownComponents}>
               {currentContent}
             </ReactMarkdown>
           </article>
